47b82dd69a6f68f35af02d45360f93f9fe7c6b09
Naveena123456 <31530611+Naveena123456@users.noreply.github.com>
2017-09-01 11:30:32 +0530

    Create posneg.c

---
+++ posneg.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+viod main()
+{
+int a=3;
+if(a>0)
+{
+printf("a is positive");
+}
+elseif(a<0)
+{
+printf("a is negative");
+}
+else
+{
+printf("a is zero");
+}
+}
