19e9257a586cd3ec55266c851df5c9bfe3862625
Naveena123456 <31530611+Naveena123456@users.noreply.github.com>
2018-01-11 16:33:12 +0530

    leap.c

---
+++ leap.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int c;
+printf("Enter the year");
+scanf("%d",&c);
+if(c%4==0)
+printf("The year is a leap year");
+else
+printf("The year is not a leap year");
+}
