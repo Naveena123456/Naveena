d1dd30b3a966d2e42b21ea9e1797ce468ad72e89
Naveena123456 <31530611+Naveena123456@users.noreply.github.com>
2018-01-11 16:25:53 +0530

    alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char c;
+printf("The character is");
+scanf("%c",&c);
+if(c>='a'&&c>='z''||c>='A'&&c>='Z')
+printf("c is an alphabet");
+else
+printf("c is not an alphabet");
+}
