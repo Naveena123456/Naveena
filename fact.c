6ea69b78832401a00de0872ad44ba333a56dc067
Naveena123456 <31530611+Naveena123456@users.noreply.github.com>
2018-01-23 16:15:02 +0530

    fact.c

---
+++ fact.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int c,n,fact=1;
+printf("Enter the number");
+scanf("%d",&n);
+for(c=1;c<=n;c++)
+{
+fact=fact*c;
+printf("The factorial of %d=%d",n,fact);
+}
+return 0;
+}
