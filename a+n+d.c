c10d35ae2247cee4695934f1e0223a0ce7611072
Naveena123456 <31530611+Naveena123456@users.noreply.github.com>
2018-01-23 16:42:33 +0530

    a+n+d.c

---
+++ a+n+d.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,n,d,sum;
+printf("Enter the numbers");
+scanf("%d",&n);
+printf("Enter the starting number");
+scanf("%d",&a);
+printf("Enter the difference");
+scanf("%d",&d);
+sum=a+n+d;
+printf("%d",sum);
+}
