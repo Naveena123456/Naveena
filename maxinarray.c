d09a6dd377a9b23100db363b725b3ac572d867f0
Naveena123456 <31530611+Naveena123456@users.noreply.github.com>
2018-01-25 16:59:04 +0530

    Create maxinarray.c

---
+++ maxinarray.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int n,a[i],max,i;
+printf("The number is");
+scanf("%d",n);
+for(i=0;i<n;i++)
+{
+printf("The array values");
+scanf("%d",&a[i]);
+max=a[0];
+}
+for(i=0;i<n;i++)
+{
+if(max<a[i])
+{
+max=a[i];
+}
+printf("%d",max);
+}
+}
