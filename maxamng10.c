32f537feb090ca046ad7609dab3e2372f2da0f91
Naveena123456 <31530611+Naveena123456@users.noreply.github.com>
2018-02-08 14:49:34 +0530

    Create maxamng10.c

---
+++ maxamng10.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+int n,a[15],i,j,temp;
+printf("The number of values");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+printf("%d",a[0]);
+}
+
