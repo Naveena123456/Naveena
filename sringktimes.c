011fb5fcdd631bced3b3790576431c24016c6038
Naveena123456 <31530611+Naveena123456@users.noreply.github.com>
2018-02-09 09:20:02 +0000

    Create sringktimes.c

---
+++ sringktimes.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char a[20];int k,i;
+printf("Num of times");
+scanf("%d",&k);
+printf("Enter the characters");
+scanf("%s",a);
+for(i=0;i<k;++i)
+{
+printf("\n %s",a);
+}
+}
+
+
