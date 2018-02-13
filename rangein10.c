9b7598b61efad6030c7891a436be184f77aa33ef
Naveena123456 <31530611+Naveena123456@users.noreply.github.com>
2018-02-13 14:29:26 +0530

    Create rangein10.c

---
+++ rangein10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("Enter the num");
+scanf("%d",&n);
+if(1<=n&&n<=10)
+{
+printf("It is in the range");
+}
+else
+{
+printf("It is not in the range");
+}
+}
