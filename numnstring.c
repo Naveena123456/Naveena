004eef3a01765163c9f4acabb5d226ebf0bd010d
Naveena123456 <31530611+Naveena123456@users.noreply.github.com>
2018-03-02 16:22:58 +0530

    Create numnstring.c

---
+++ numnstring.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+char a[100];
+int i;
+printf("Enter input");
+scanf("%s",a);
+if('a'<=a[i]&&a[i]<='z'&&'A'<=a[i]&&a[i]<='Z'&&'0'<=a[i]&&a[i]<='9')
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
+}
